c0d1384d959d2cec6b50b3341fdc7a39102ba550
Zachary Hancock <zachbhancock@gmail.com>
2025-01-13 16:07:27 -0500

    new

---
+++ R/sandwrm-package.R
@@ -0,0 +1,16 @@
+#' The 'sandwrm' package.
+#'
+#' @description Bayesian estimation of parameters of the Wright-Malecot model. dataBlock is prepared as below, nChains is the number of independent MCMC chains to run, nIter is how many steps per chain, and prefix is the name that will be affixed to the output files.
+#'
+#' @docType package
+#' @name sandwrm-package
+#' @aliases sandwrm-package
+#' @import rstantools
+#' @import fields
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Z.B. Hancock, R.H. Toczydlowski, and G.S. Bradburd (2024) <doi: 10.1093/genetics/iyae094>.
+#'
+#' Stan Development Team (2018). RStan: the R interface to Stan. R package version 2.17.3. http://mc-stan.org
+NULL
