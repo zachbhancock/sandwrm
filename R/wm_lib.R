b54d31e02c3c0e271b21cbe432ba5f9537bcb0db
Zach Hancock <103067043+zachbhancock@users.noreply.github.com>
2025-05-05 13:22:06 -0400

    Update wm_lib.R

--- R/wm_lib.R
+++ R/wm_lib.R
@@ -19,8 +19,8 @@ sandwrm <- function(stanMod=ibsMod,dataBlock,nChains,nIter,prefix,MLjumpstart=FA
     }
     initPars <- lapply(1:nChains,function(i){ml2init(db=dataBlock,mod=stanMod,nRuns=nMLruns,Gmodel=Gmodel,prefix=prefix)})
   } else {
-    dataBlock <- 1 - dataBlock
-    diag(dataBlock) <- 1
+    dataBlock$hom <- 1 - dataBlock$hom
+    diag(dataBlock$hom) <- 1
     initPars <- lapply(1:nChains,function(i){generateInitPars(dataBlock=dataBlock,nChains=nChains,Gmodel=Gmodel,prefix=prefix)})
   }
   calling <- r"{
