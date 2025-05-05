9fc927e942d411ba64012a168a9ecb83fa559ea2
Zach Hancock <103067043+zachbhancock@users.noreply.github.com>
2025-05-05 13:31:04 -0400

    Update wm_lib.R

--- R/wm_lib.R
+++ R/wm_lib.R
@@ -13,8 +13,8 @@ source(stanFile)
 ibsMod <- stan_model(model_code=stanBlock)
 
 sandwrm <- function(stanMod=ibsMod,dataBlock,nChains,nIter,prefix,MLjumpstart=FALSE,nMLruns=NULL,Gmodel=FALSE){
-  dataBlock$hom <- 1 - dataBlock$hom
-  diag(dataBlock$hom) <- 1
+  dataBlock[3] <- 1 - dataBlock[3]
+  diag(dataBlock[3]) <- 1
   if(MLjumpstart){
     if(is.null(nMLruns)){
       stop("\nyou must specify the number of maximum liklihood jumpstart runs to perform\n")
